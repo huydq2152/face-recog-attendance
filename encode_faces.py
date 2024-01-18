013137d10cab721211d4dfb3e8607ed838f7b891
huy <huy.dq1@bimgroup.com>
2024-01-18 15:15:43 +0700

    split to compare to image of just current person

--- encode_faces.py
+++ encode_faces.py
@@ -1,6 +1,6 @@
-""" Để lưu encodings và tên của các faces trong dataset vào file encodings.pickle"""
+""" Để lưu encodings và tên của các faces trong dataset vào file có tên tương ứng trong folder encodings"""
 # USAGE
-# python encode_faces.py --dataset dataset --encodings encodings.pickle
+# python encode_faces.py --dataset dataset --encodings encodings
 
 from imutils import paths
 import argparse
@@ -11,56 +11,67 @@ import face_recognition
 
 ap =argparse.ArgumentParser()
 ap.add_argument("-i", "--dataset", required=True, help="path to the directory of faces and images")
-# các encodings và names được lưu vào file
+# các encodings và names được lưu vào file tương ứng trong folder
 ap.add_argument("-e", "--encodings", required=True, help="path to the serrialized db of facial encoding")  
 # trước khi encode face thì phải detect nó (đây là bước luôn phải làm trong face recognition) - chọn method để detect faces
 ap.add_argument("-d", "--detection_method", type=str, default="hog", help="face detector to use: cnn or hog")
 args = vars(ap.parse_args())
 
-# lấy paths của images trong dataset
-print("[INFO] quantifying faces...")
-imagePaths = list(paths.list_images(args["dataset"]))
+# lấy tên các folder trong dataset
+folders = os.listdir(args["dataset"])
 
-# khởi tạo list chứa known encodings và known names (để các test images so sánh)
-# chứa encodings và tên của các images trong dataset
-knownEncodings = []
-knownNames = []
+for folder in folders:
+    folder_path = os.path.join(args["dataset"], folder)
 
-# duyệt qua các image paths
-for (i, imagePath) in enumerate(imagePaths):
-    # lấy tên người từ imagepath
-    print("[INFO] processing image {}/{}".format(i+1, len(imagePaths)))
-    name = imagePath.split(os.path.sep)[-2]
+    if os.path.isdir(folder_path):
+        # lấy paths của images trong dataset
+        print("[INFO] quantifying faces...")
+        imagePaths = list(paths.list_images(folder_path))
 
-    # load image bằng OpenCV và chuyển từ BGR (thứ tự của OpenCV) sang RGB (thứ tự của cần)
-    image = cv2.imread(imagePath)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # khởi tạo list chứa known encodings và known names (để các test images so sánh)
+        # chứa encodings và tên của các images trong dataset
+        folder_name = os.path.basename(folder_path)
+        knownEncodings = []
+        knownNames = []
 
-    # Đối với từng image phải thực hiện detect face, trích xuất face ROI và chuyển về encoding
-    # trả về array of bboxes of faces, dùng dlib như bài face detection đó
-    # model="cnn" chính xác hơn nhưng chậm hơn, "hog" nhanh hơn nhưng kém chính xác hơn
-    boxes = face_recognition.face_locations(rgb, model=args["detection_method"])    
+        # duyệt qua các image paths
+        for (i, imagePath) in enumerate(imagePaths):
+            # lấy tên người từ imagepath
+            print("[INFO] processing image {}/{}".format(i+1, len(imagePaths)))
+            name = imagePath.split(os.path.sep)[-2]
 
-    # tính the facial embedding for the face
-    # sẽ tính encodings cho mỗi face phát hiện được trong ảnh (có thể có nhiều faces)
-    # Để lý tưởng trong ảnh nên chỉ có một mặt người của mình thôi
-    encodings = face_recognition.face_encodings(rgb, boxes)  
+            # load image bằng OpenCV và chuyển từ BGR (thứ tự của OpenCV) sang RGB (thứ tự của cần)
+            image = cv2.imread(imagePath)
+            rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # duyệt qua các encodings
-    # Trong ảnh có thể có nhiều faces, mà ở đây chỉ có 1 tên
-    # Nên chắc chắn trong dataset ban đầu ảnh chỉ có một mặt người thôi nhé
-    # Lý tưởng nhất mỗi ảnh có 1 face và có 1 encoding thôi
-    for encoding in encodings:
-        # lưu encoding và name vào lists bên trên
-        knownEncodings.append(encoding)
-        knownNames.append(name)
+            # Đối với từng image phải thực hiện detect face, trích xuất face ROI và chuyển về encoding
+            # trả về array of bboxes of faces, dùng dlib như bài face detection đó
+            # model="cnn" chính xác hơn nhưng chậm hơn, "hog" nhanh hơn nhưng kém chính xác hơn
+            boxes = face_recognition.face_locations(rgb, model=args["detection_method"])    
 
-# dump (lưu) the facial encodings + names vào ổ cứng
-print("[INFO] serializing encodings...")
-data = {"encodings": knownEncodings, "names": knownNames}
+            # tính the facial embedding for the face
+            # sẽ tính encodings cho mỗi face phát hiện được trong ảnh (có thể có nhiều faces)
+            # Để lý tưởng trong ảnh nên chỉ có một mặt người của mình thôi
+            encodings = face_recognition.face_encodings(rgb, boxes)  
 
-with open(args["encodings"], "wb") as f:
-    f.write(pickle.dumps(data))
+            # duyệt qua các encodings
+            # Trong ảnh có thể có nhiều faces, mà ở đây chỉ có 1 tên
+            # Nên chắc chắn trong dataset ban đầu ảnh chỉ có một mặt người thôi nhé
+            # Lý tưởng nhất mỗi ảnh có 1 face và có 1 encoding thôi
+            for encoding in encodings:
+                # lưu encoding và name vào lists bên trên
+                knownEncodings.append(encoding)
+                knownNames.append(name)
+
+        # dump (lưu) the facial encodings + names vào ổ cứng
+        print("[INFO] serializing encodings...")
+        data = {"encodings": knownEncodings, "names": knownNames}
+
+        # lưu vào file có tên tương ứng với folder trong folder encodings
+        # vd: dataset/1 -> encodings/1.pickle 
+        pickle_file_path = os.path.join(args["encodings"], f"{folder_name}.pickle")
+        with open(pickle_file_path, "wb") as f:
+            f.write(pickle.dumps(data))
 
 
     
--- recognize_faces_image.py
+++ recognize_faces_image.py
@@ -1,28 +1,31 @@
 # USAGE
-# python recognize_faces_image.py --encodings encodings.pickle --image 1.png
+# python recognize_faces_image.py --personId 1 --encodings encodings --image .\test_images\toi.png 
+import os
 import face_recognition
 import argparse
 import pickle
 import cv2
 
 ap = argparse.ArgumentParser()
-# đường dẫn đến file encodings đã lưu
+# đường dẫn đến folder encodings chứa file pickle đã lưu
+ap.add_argument("-pId", "--personId", required=True, help="person id for recognition")
 ap.add_argument("-e", "--encodings", required=True, help="path to the serialized db of facial encodings")
 ap.add_argument("-i", "--image", required=True, help="path to the test image")
 # nếu chạy trên CPU hay embedding devices thì để hog, còn khi tạo encoding vẫn dùng cnn cho chính xác
 ap.add_argument("-d", "--detection_method", type=str, default="hog", help="face dettection model to use: cnn or hog")
 args = vars(ap.parse_args())
 
-# load the known faces and encodings
+# load encodings đã lưu từ file
 print("[INFO] loading encodings...")
-data = pickle.loads(open(args["encodings"], "rb").read())      # loads - load từ file
+id = args["personId"];
+pickle_file_path = os.path.join(args["encodings"], f"{id}.pickle")
+data = pickle.loads(open(pickle_file_path, "rb").read())     
 
 # load image và chuyển từ BGR to RGB (dlib cần để chuyển về encoding)
 image = cv2.imread(args["image"])
 rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# CŨng làm tương tự cho ảnh test, detect face, extract face ROI, chuyển về encoding
-# rồi cuối cùng là so sánh kNN để recognize
+# detect faces trong ảnh và trích xuất các encodings
 print("[INFO] recognizing faces...")
 boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
 encodings = face_recognition.face_encodings(rgb, boxes)
@@ -36,26 +39,14 @@ for encoding in encodings:
     # khớp encoding của từng face phát hiện được với known encodings (từ datatset)
     # so sánh list of known encodings và encoding cần check, sẽ trả về list of True/False xem từng known encoding có khớp với encoding check không
     # có bao nhiêu known encodings thì trả về list có bấy nhiêu phần tử
-    # trong hàm compare_faces sẽ tính Euclidean distance và so sánh với tolerance=0.6 (mặc định), nhó hơn thì khớp, ngược lại thì ko khớp (khác người)
-    matches = face_recognition.compare_faces(data["encodings"], encoding, 0.4)      # có thể điều chỉnh tham số cuối
-    name = "Unknown"    # tạm thời vậy, sau này khớp thì đổi tên
+    # trong hàm compare_faces sẽ tính Euclidean distance và so sánh với tolerance=0.6 (mặc định), nhó hơn thì khớp, ngược lại thì ko khớp (khác người), tham số cuối càng nhỏ càng strict
+    match = any(face_recognition.compare_faces(data["encodings"], encoding, 0.3))      
+    name = "Unknown"    
 
     # Kiểm tra xem từng encoding có khớp với known encodings nào không,
-    if True in matches:
-        # lưu các chỉ số mà encoding khớp với known encodings (nghĩa là b == True)
-        matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-
-        # tạo dictionary để đếm tổng số lần mỗi face khớp
-        counts = {}
-        # duyệt qua các chỉ số được khớp và đếm số lượng 
-        for i in matchedIdxs:
-            name = data["names"][i]     # tên tương ứng known encoding khiowps với encoding check
-            counts[name] = counts.get(name, 0) + 1  # nếu chưa có trong dict thì + 1, có rồi thì lấy số cũ + 1
-
-        # lấy tên có nhiều counts nhất (tên có encoding khớp nhiều nhất với encoding cần check)
-        # có nhiều cách để có thể sắp xếp list theo value ví dụ new_dic = sorted(dic.items(), key=lambda x: x[1], reverse=True)
-        # nó sẽ trả về list of tuple, mình chỉ cần lấy name = new_dic[0][0]
-        name = max(counts, key=counts.get)
+    if match:
+        # Nếu có khớp, thì đặt tên là tên trong dataset
+        name = data["names"][0]  # Lấy tên từ dataset (giả sử chỉ có một tên trong dataset)
 
     names.append(name)
 
